36e08289726311322c27fd738cbe007eaf9c2b7b
Nic Haynes <nic@nicinabox.com>
2012-06-24 10:32:21 -0500

    Add gaug.es warning and remove parameterized urls

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,16 +1,7 @@
-var parameterize = function(str) {
-  return str.toLowerCase().replace(/['\.,]/g, '').replace(/\s/g, '-')
-}
-
 $(document).ready(function() {
   $('#slides').superslides({
     play: true,
     slide_easing: 'easeInOutCubic',
     slide_speed: 800
-  })
-  .on('slides.animated', function(e) {
-    title = parameterize($('.current h1').text())
-    History.pushState({ page: title }, null, title);
-    _gauges.push(['track']);
   });
 });
