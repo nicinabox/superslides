ea4fa358e5df06cbffe115933065a2d020796eeb
Nic Haynes <nic@nicinabox.com>
2012-08-11 14:36:02 -0500

    Update with 0.4

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#slides').superslides({
     play: true,
     slide_easing: 'easeInOutCubic',
-    slide_speed: 800
+    slide_speed: 800,
+    pagination: true
   });
 });
