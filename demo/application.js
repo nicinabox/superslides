05a93bd5645442f50c64f5a3c9c6c114fd49bd41
Nic Haynes <nic@nicinabox.com>
2012-10-22 13:33:10 -0500

    Remove spec, add demo

---
+++ demo/application.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('#slides').superslides({
+    play: true,
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true
+  });
+
+  // Update verion based on github tags
+  var url = 'https://api.github.com/repos/nicinabox/superslides/git/refs/tags';
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(json) {
+      data = json.data;
+      var version = data.pop().ref.split('/').pop();
+      $('.version').html(version);
+    }
+  });
+
+});
