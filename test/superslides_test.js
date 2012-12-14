720fe330cde77abc05ff2d00771d9ac839a718b2
Nic Haynes <nic@nicinabox.com>
2012-12-14 13:25:43 -0600

    Remove old code

--- test/superslides_test.js
+++ test/superslides_test.js
@@ -1,7 +1,7 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
 /*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
-// $slides = [];
+
 (function($) {
   var testSetup = {
     testStart: function(details) {
