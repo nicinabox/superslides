d9927217d414d9b615a20020a79fed65de95d149
Nic Haynes <nic@nicinabox.com>
2012-12-14 23:57:58 -0600

    Update demo

--- demo/javascripts/application.js
+++ demo/javascripts/application.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#slides').superslides({
     slide_easing: 'easeInOutCubic',
     slide_speed: 800,
-    pagination: true
+    pagination: true,
+    hashchange: true
   });
 });
