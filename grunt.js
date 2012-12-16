f211e23357607c66e2db0548cae83731cb2562cb
Nic Haynes <nic@nicinabox.com>
2012-12-16 12:27:52 -0600

    Don't use bare coffeescript

--- grunt.js
+++ grunt.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
         src: ['src/**/*.coffee'],
         dest: 'dist',
         options: {
-          bare: true
+          bare: false
         }
       }
     },
