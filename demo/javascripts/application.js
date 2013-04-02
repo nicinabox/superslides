ad592d1880dc037122c37200fb794a8117f697a6
Nic Haynes <nic@nicinabox.com>
2013-04-02 17:37:42 -0500

    Update demo code from testing

--- demo/javascripts/application.js
+++ demo/javascripts/application.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  $(document).on('init.slides', function() {
+    additional_slides = $('#additional-slides').html();
+    $(additional_slides).appendTo('#slides .slides-container');
+    $('#slides').superslides('update');
+  });
+
   $('#slides').superslides({
     slide_easing: 'easeInOutCubic',
     slide_speed: 800,
@@ -6,4 +12,5 @@ $(document).ready(function() {
     hashchange: true,
     scrollable: true
   });
+
 });
